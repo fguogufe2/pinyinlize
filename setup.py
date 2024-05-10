f7077021d44e389ec42f7e7ac6f61e5e4a159ed6
Fei Guo <32278690+fguogufe2@users.noreply.github.com>
2024-05-10 15:31:56 -0500

    c

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='my_package',
+    name='pinyinlize',
     version='0.1',
     packages=find_packages(),
     install_requires=[
-        "jieba", "pypinyin"
+        "jieba >= 0.42.1", 
+        "pypinyin >= 0.50.0"
     ],
     entry_points={
         'console_scripts': [
             'pinyinlize=pinyinlize.pinpinlize:main'
         ]
-    }
+    },
     auhtor='Fei Guo',
-    author_email=fguogufe2@gmail.com
-    description='A package for Chinese text processing'
-    license='MIT'
-    keywords='Chinese text processing'
+    author_email= "fguogufe2@gmail.com",
+    description='a package converts short Chinese-character strings(titles of books, articles or journals) to pinyin.',
+    license='MIT',
+    keywords='convert chinese text to pinyin'
 )
